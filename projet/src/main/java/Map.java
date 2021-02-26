c544485deba8696f7e7c7bc8c0925f69d9f757cf
RodriguePoisson <rodrigue.poisson@etu.unice.fr>
2021-02-26 13:11:02 +0100

    test

--- projet/src/main/java/Map.java
+++ projet/src/main/java/Map.java
@@ -3,12 +3,16 @@ public class Map {
     private int y;
     private char symbole;
 
-    public Map(){
+    public Map()
+    {
     }
 
-    public void dessine(){
-        for (int i = 0; i < 15; i++) {
-            for (int j = 0; j < 30; j++){
+    public void dessine()
+    {
+        for (int i = 0; i < 15; i++)
+        {
+            for (int j = 0; j < 30; j++)
+            {
                 System.out.print(".  ");
             }
             System.out.println("");
---
+++ projet/src/main/java/Salle.java
@@ -0,0 +1,10 @@
+public class Salle
+{
+    private int largeurSalle;
+    private int longueurSalle;
+    public Salle(int largeurSalle,int longueurSalle)
+    {
+        this.largeurSalle = largeurSalle;
+        this.longueurSalle = longueurSalle;
+    }
+}
