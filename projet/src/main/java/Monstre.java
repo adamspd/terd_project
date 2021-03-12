99ec6bcb111a32adb5380c04a8fc754d62a5d43e
davidPRIGODIN <79266421+davidPRIGODIN@users.noreply.github.com>
2021-03-12 14:10:37 +0100

    Add files via upload

---
+++ projet/src/main/java/Monstre.java
@@ -0,0 +1,37 @@
+public class Monstre {
+    public string REPRESENTATION;  //Symbol
+    public int PV;
+    public int DEGAT;
+    //public int vitesse_de_deplacement;
+    public int pos_x;
+    public int pos_y;
+
+
+    //if (grille.Get_LARGEURGRILLE()); //LARGEURGRILLE est privé donc il nous faut une méthode Get_LARGEURGRILLE()
+    public void Se_deplacer_en_bas(/*String[][] grille*/) {
+
+
+    }
+    public void Se_deplacer_a_droite() {
+
+
+    }
+
+
+
+    //-------------------------------------------------------------------------------------
+    public void Se_deplacer_en_haut() {/* -Se_Deplacer_en_bas(); */}
+    public void Se_deplacer_a_gauche() {/* -Se_Deplacer_a_droite(String[][] grille); */}
+   /*  Se deplacer en diagonal ...
+   diagonal_haut_gauche()
+   diagonal_haut_droite()
+   diagonal_bas_gauche()
+   diagonal_bas_droite()
+    */
+
+    public void Attaquer(/*Joueur joueur*/) {/*joueur.PV-DEGAT;*/}
+
+
+
+}
+
